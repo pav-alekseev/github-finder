080f588a325605bd0e8861fc22df9bec6c6dcf56
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-08 15:05:15 +0300

    Add fetching users from Github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import './App.css';
@@ -6,11 +8,23 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchGithubUser = async () => {
+      const { data: fetchedUsers } = await axios.get(
+        'https://api.github.com/users'
+      );
+      setUsers(fetchedUsers);
+    };
+    fetchGithubUser();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <Users />
+        <Users users={users} />
       </div>
     </div>
   );
