94d38cee44b8e51173d15dbde473e6b77c176a98
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-08 14:34:53 +0300

    Add Users component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
---
+++ src/components/users/Users.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: '1',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+      {
+        id: '2',
+        login: 'defunkt',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+        html_url: 'https://github.com/defunkt',
+      },
+      {
+        id: '3',
+        login: 'pjhyett',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+        html_url: 'https://github.com/pjhyett',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="grid-3">
+        {this.state.users.map(user => (
+          <UserItem user={user} key={user.id} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Users;
