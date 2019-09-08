10f7c2de47e52279c784ff8eac585ad27fa684ba
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-08 13:55:39 +0300

    Add package for icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Navbar />
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
 
