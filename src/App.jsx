919ce406512e9d1ce3d5ea2de6dca7cef0357884
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-08 13:57:41 +0300

    Add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
   }
 }
 
