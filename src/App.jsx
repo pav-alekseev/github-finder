c1d4247aa242586ce0b8d516ccdbc602b6a62e62
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-08 13:20:24 +0300

    Clean up template code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
