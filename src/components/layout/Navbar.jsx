69d7c73133c4294c69437371ea977347c1af9d28
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-11 15:53:24 +0300

    Add screen links to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => (
   <nav className="navbar bg-primary">
     <h1>
       <i className={icon} /> {title}
     </h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
   </nav>
 );
 
