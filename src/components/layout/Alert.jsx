d22943a68db286a950de5f9797373f516f2eb6e2
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-11 15:14:13 +0300

    Add alert component

---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Alert = ({ alert }) => {
+  return (
+    alert && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle" />
+        {` ${alert.message}`}
+      </div>
+    )
+  );
+};
+
+export default Alert;
