4b567db17ca5dad53038dce6e76382c507867f8a
Pavel Alekseev <pavelekseev@gmail.com>
2019-09-19 22:00:12 +0300

    Add basic github state

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USER,
+  GET_REPOS,
+  SET_LOADING,
+} from '../types';
+
+const GithubState = props => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
