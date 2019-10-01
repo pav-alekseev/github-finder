39c8a3d54212119af0910287853a7c965f028ed4
Pavel Alekseev <pavelekseev@gmail.com>
2019-10-01 22:08:48 +0300

    Use Home component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -8,10 +8,8 @@ import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
 import Navbar from './components/layout/Navbar';
-import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import User from './components/users/User';
+import Home from './screens/Home';
 import About from './screens/About';
 
 import './App.css';
@@ -24,17 +22,7 @@ const App = () => (
           <Navbar />
           <div className="container">
             <Switch>
-              <Route
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Alert />
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-                exact
-              />
+              <Route path="/" component={Home} exact />
               <Route path="/about" component={About} />
               <Route path="/user/:login" component={User} />
             </Switch>
